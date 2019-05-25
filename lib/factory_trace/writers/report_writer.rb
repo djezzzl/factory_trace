395faa6779bafe23ea6ef09bc2ebc30d6b4a4209
djezzzl <lawliet.djez@gmail.com>
2019-05-25 21:47:20 +0200

    Fix ReportWriter class

--- lib/factory_trace/writers/report_writer.rb
+++ lib/factory_trace/writers/report_writer.rb
@@ -1,41 +1,43 @@
 module FactoryTrace
-  class ReportWriter < Writer
-    COLORS = {
-      blue: "\e[34m",
-      green: "\e[32m",
-      red: "\e[31m"
-    }.freeze
+  module Writers
+    class ReportWriter < Writer
+      COLORS = {
+        blue: "\e[34m",
+        green: "\e[32m",
+        red: "\e[31m"
+      }.freeze
 
-    # @param [Array<Hash>] results
-    def write(results)
-      total_color = results.size == 2 ? :green : :red
+      # @param [Array<Hash>] results
+      def write(results)
+        total_color = results.size == 2 ? :green : :red
 
-      results.each do |result|
-        io.puts(convert(result, total_color: total_color))
+        results.each do |result|
+          configuration.out.puts(convert(result, total_color: total_color))
+        end
       end
-    end
 
-    private
+      private
 
-    # @param [Hash<Symbol, Object>] result
-    # @param [Symbol] total_color
-    def convert(result, total_color:)
-      case
-      when result[:value]
-        colorize(total_color, "total number of unique #{result[:code]} factories & traits: #{result[:value]}")
-      when result[:factory_name] && result[:trait_name]
-        "#{result[:code]} trait #{colorize(:blue, result[:trait_name])} of factory #{colorize(:blue, result[:factory_name])}"
-      when result[:factory_name]
-        "#{result[:code]} factory #{colorize(:blue, result[:factory_name])}"
-      else
-        "#{result[:code]} global trait #{colorize(:blue, result[:trait_name])}"
+      # @param [Hash<Symbol, Object>] result
+      # @param [Symbol] total_color
+      def convert(result, total_color:)
+        case
+        when result[:value]
+          colorize(total_color, "total number of unique #{result[:code]} factories & traits: #{result[:value]}")
+        when result[:factory_name] && result[:trait_name]
+          "#{result[:code]} trait #{colorize(:blue, result[:trait_name])} of factory #{colorize(:blue, result[:factory_name])}"
+        when result[:factory_name]
+          "#{result[:code]} factory #{colorize(:blue, result[:factory_name])}"
+        else
+          "#{result[:code]} global trait #{colorize(:blue, result[:trait_name])}"
+        end
       end
-    end
 
-    def colorize(color, msg)
-      return msg unless config.color
+      def colorize(color, msg)
+        return msg unless configuration.color
 
-      "#{COLORS[color]}#{msg}\e[0m"
+        "#{COLORS[color]}#{msg}\e[0m"
+      end
     end
   end
 end
