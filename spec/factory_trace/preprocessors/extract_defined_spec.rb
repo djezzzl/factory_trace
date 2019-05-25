ceb5047e147b210f223c81213dceb494f6adcb64
djezzzl <lawliet.djez@gmail.com>
2019-05-25 19:07:20 +0200

    Add ExtractDefined preprocessor

--- spec/factory_trace/preprocessors/extract_defined_spec.rb
+++ spec/factory_trace/preprocessors/extract_defined_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe FactoryTrace::Preprocessors::ExtractDefined do
 
     specify do
       collection = FactoryTrace::Structures::Collection.new(
-        [
-          FactoryTrace::Structures::Factory.new('user', nil, ['with_phone']),
-          FactoryTrace::Structures::Factory.new('admin', 'user', ['with_email']),
-          FactoryTrace::Structures::Factory.new('company', nil, []),
-        ],
-        [
-          FactoryTrace::Structures::Trait.new('with_address', nil)
-        ]
+        {
+          'user' => FactoryTrace::Structures::Factory.new('user', nil, ['with_phone']),
+          'admin' => FactoryTrace::Structures::Factory.new('admin', 'user', ['with_email']),
+          'company' => FactoryTrace::Structures::Factory.new('company', nil, [])
+        },
+        {
+          'with_address' => FactoryTrace::Structures::Trait.new('with_address', nil)
+        }
       )
 
       expect(subject).to eq(collection)
