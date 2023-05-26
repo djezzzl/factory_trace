b667da7c761ae819bdd1fe8fecbffac0e41d9338
djezzzl <lawliet.djez@gmail.com>
2023-05-26 09:38:44 +0200

    Add usage of a factory

---
+++ spec/integration_tests/test_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe "integration test" do # rubocop:disable RSpec/DescribeClass
+  it "uses some" do # rubocop:disable Rspec/NoExpectationExample
+    build(:admin)
+  end
+end
