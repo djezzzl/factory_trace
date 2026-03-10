c7ada3184de28a6f28f73d54d7ea2d0c5442ecb2
djezzzl <lawliet.djez@gmail.com>
2026-03-10 09:25:39 +0800

    Extract integration tests

---
+++ integration_tests/rspec/spec_helper.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "bundler/setup"
+require "factory_trace"
+
+require "tempfile"
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryBot.find_definitions
+  end
+end
